2878a1391b0606665626ca8b89121f8bc0fcfe4e
SonJJ <zzzzax@naver.com>
2018-04-20 15:28:30 +0900

    2번 완료

--- java-framework-class-miexam-master/java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/ProductDao.java
+++ java-framework-class-miexam-master/java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/ProductDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu;
 
 import java.sql.*;
 
-public class ProductDao {
+public abstract class ProductDao {
     public Product get(Long id) throws ClassNotFoundException, SQLException {
         Connection connection = getConnection();
 
@@ -45,9 +45,9 @@ public class ProductDao {
 
         return id;
     }
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost/jeju?characterEncoding=utf-8", "root", "root12345");
-    }
+    abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+//        Class.forName("com.mysql.jdbc.Driver");
+//        return DriverManager.getConnection(
+//                "jdbc:mysql://localhost/jeju?characterEncoding=utf-8", "root", "root12345");
+
 }
--- java-framework-class-miexam-master/java-framework-class-miexam-master/src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ java-framework-class-miexam-master/java-framework-class-miexam-master/src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -8,31 +8,57 @@ import java.sql.SQLException;
 import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
-    private ProductDao productDao;
+    private JejuProductDao jejuProductDao;
+    private HallaProductDao hallaProductDao;
     @Before
     public void setup() {
-        productDao = new ProductDao();
+        jejuProductDao= new JejuProductDao();
+        hallaProductDao = new HallaProductDao();
     }
 
     @Test
-    public void get() throws SQLException, ClassNotFoundException {
+    public void jejuGet() throws SQLException, ClassNotFoundException {
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
 
-        Product product = productDao.get(id);
+        Product product = jejuProductDao.get(id);
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
     }
     @Test
-    public void add() throws SQLException, ClassNotFoundException {
+    public void jejuAdd() throws SQLException, ClassNotFoundException {
         Product product = new Product();
         product.setTitle("가나다라");
         product.setPrice(111111);
-        Long id = productDao.insert(product);
+        Long id = jejuProductDao.insert(product);
 
-        Product insertProduct = productDao.get(id);
+        Product insertProduct = jejuProductDao.get(id);
+        assertEquals(id, insertProduct.getId());
+        assertEquals(product.getTitle(), insertProduct.getTitle());
+        assertEquals(product.getPrice(), insertProduct.getPrice());
+    }
+
+    @Test
+    public void hallaGet() throws SQLException, ClassNotFoundException {
+        Long id = 1L;
+        String title = "제주감귤";
+        Integer price = 15000;
+
+        Product product = hallaProductDao.get(id);
+        assertEquals(id, product.getId());
+        assertEquals(title, product.getTitle());
+        assertEquals(price, product.getPrice());
+    }
+    @Test
+    public void hallaAdd() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        product.setTitle("가나다라");
+        product.setPrice(111111);
+        Long id = hallaProductDao.insert(product);
+
+        Product insertProduct = hallaProductDao.get(id);
         assertEquals(id, insertProduct.getId());
         assertEquals(product.getTitle(), insertProduct.getTitle());
         assertEquals(product.getPrice(), insertProduct.getPrice());
